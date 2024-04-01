59ed57097fd8ca436a441fb33a295cfe5fb3f011
Owen Myers <beanswitheggs@gmail.com>
2024-04-01 11:53:24 -0500

    Methods created

---
+++ src/studentdriver/StudentFees.java
@@ -0,0 +1,50 @@
+package studentdriver;
+
+public abstract class StudentFees {
+    String studentName;
+    int studentID;
+    boolean isEnrolled;
+    int CREDITS_PER_COURSE = 3;
+    double PER_CREDIT_FEE = 543.50;
+
+    public StudentFees(String studentName, int studentID, boolean isEnrolled) {
+
+    }
+    public String getStudentName(){
+        return studentName;
+    }
+
+    public int getStudentID(){
+        return studentID;
+    }
+
+    public boolean isIsEnrolled(){
+        return isEnrolled;
+    }
+
+    public double getCREDITS_PER_COURSE(){
+        return CREDITS_PER_COURSE;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public void IsEnrolled(boolean isEnrolled) {
+        this.isEnrolled = isEnrolled;
+    }
+
+    public double getPayableAmount(){
+        return CREDITS_PER_COURSE * PER_CREDIT_FEE;
+    }
+
+    //May need to remake
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
